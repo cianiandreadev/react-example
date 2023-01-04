d79d5b1c1b212bba082b233b52e2235b79a564b5
Andrea Ciani <cianiandrea@icloud.com>
2023-01-04 21:48:42 +0100

    Testing people apis

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,46 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button'
+import useSWR from 'swr'
 
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Ciani Andrea - Starwars API</title>
-        <meta name="description" content="Interview project by Andrea Ciani" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome
-        </h1>
+    const { data, error, isLoading } = useSWR('https://swapi.dev/api/people/1', fetcher)
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Example Card</h2>
-            <p>Let's keep this for later</p>
-          </a>
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+    console.log(data)
+    return <div>hello {data.name}!</div>
+
+    return (
+
+        <div className={styles.container}>
+            <Head>
+                <title>Ciani Andrea - Starwars API</title>
+                <meta name="description" content="Interview project by Andrea Ciani" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome
+                </h1>
+
+                <div>
+                    <Button variant="contained">Hello World</Button>
+                </div>
+
+                <div className={styles.grid}>
+                    <a href="https://nextjs.org/docs" className={styles.card}>
+                        <h2>Example Card</h2>
+                        <p>Let's keep this for later</p>
+                    </a>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                No copyright for this example project!
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        No copyright for this example project!
-      </footer>
-    </div>
-  )
+    )
 }
